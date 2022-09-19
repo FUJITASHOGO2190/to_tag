d66782702ec911cfd51646435cd84bb47370b80c
FUJITASHOGO2190 <mr.shouuge@gmail.com>
2022-09-19 18:56:25 +0900

    投稿機能追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,20 @@ class PostsController < ApplicationController
   def index
     @post = Post.all
   end
+
+  def new
+   @post = Post.new 
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to root_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:text, :image)
+  end
 end
